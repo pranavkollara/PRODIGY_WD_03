dea9099acc53b03eb152094dc90d00a0c18a0b7c
pranavkollara <kollarapranav@gmail.com>
2023-12-17 15:05:03 +0530

    audio and stuff

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ let grid7 = document.getElementById("grid7");
 let grid8 = document.getElementById("grid8");
 let grid9 = document.getElementById("grid9");
 let startBtn = document.getElementById("start");
+let resetBtn = document.getElementById("reset")
+let winaudio = new Audio("win.mp3")
+let begin = new Audio("start.mp3")
+let act = new Audio("move.mp3")
+let draw = new Audio("draw.mp3")
 
 let g1;
 let g2;
@@ -23,10 +28,20 @@ let turn = ["X", "O"];
 
 let i = 0;
 
-
 let game = false;
 
+let winner =0;
+
+resetBtn.addEventListener('click',()=>{
+  location.reload();
+})
+
 startBtn.addEventListener('click',()=>{
+  begin.play();
+  startDiv.classList.add("hidden");
+  resetDiv.classList.remove("hidden");
+  display.classList.remove("hidden");
+
     grid1.classList.remove("disable");
     grid2.classList.remove("disable");
     grid3.classList.remove("disable");
@@ -38,44 +53,54 @@ startBtn.addEventListener('click',()=>{
     grid9.classList.remove("disable");
     game = true;
     if (game) {
+      
         grid1.addEventListener("click", () => {
+          act.play();
           document.getElementById("grid1").innerHTML = turn[i % 2];
           g1 = turn[i % 2];
           i++;
           grid1.classList.remove("hidden");
           grid1.classList.add("used");
+          document.getElementById("symbol").innerHTML=turn[i%2]
           win();
           
         });
       
         grid2.addEventListener("click", () => {
+          act.play();
           document.getElementById("grid2").innerHTML = turn[i % 2];
           g2 = turn[i % 2];
           i++;
           grid2.classList.remove("hidden");
           grid2.classList.add("used");
+          document.getElementById("symbol").innerHTML=turn[i%2]
           win();
         });
       
         grid3.addEventListener("click", () => {
+          act.play();
           document.getElementById("grid3").innerHTML = turn[i % 2];
           g3 = turn[i % 2];
           i++;
           grid3.classList.remove("hidden");
           grid3.classList.add("used");
+          document.getElementById("symbol").innerHTML=turn[i%2]
           win();
         });
       
         grid4.addEventListener("click", () => {
+          act.play();
           document.getElementById("grid4").innerHTML = turn[i % 2];
           g4 = turn[i % 2];
           i++;
           grid4.classList.remove("hidden");
           grid4.classList.add("used");
+          document.getElementById("symbol").innerHTML=turn[i%2]
           win();
         });
       
         grid5.addEventListener("click", () => {
+          act.play();
           document.getElementById("grid5").innerHTML = turn[i % 2];
           g5 = turn[i % 2];
           i++;
@@ -84,38 +109,46 @@ startBtn.addEventListener('click',()=>{
         });
       
         grid6.addEventListener("click", () => {
+          act.play();
           document.getElementById("grid6").innerHTML = turn[i % 2];
           g6 = turn[i % 2];
           i++;
           grid6.classList.remove("hidden");
           grid6.classList.add("used");
+          document.getElementById("symbol").innerHTML=turn[i%2]
           win();
         });
       
         grid7.addEventListener("click", () => {
+          act.play();
           document.getElementById("grid7").innerHTML = turn[i % 2];
           g7 = turn[i % 2];
           i++;
           grid7.classList.remove("hidden");
           grid7.classList.add("used");
+          document.getElementById("symbol").innerHTML=turn[i%2]
           win();
         });
       
         grid8.addEventListener("click", () => {
+          act.play();
           document.getElementById("grid8").innerHTML = turn[i % 2];
           g8 = turn[i % 2];
           i++;
           grid8.classList.remove("hidden");
           grid8.classList.add("used");
+          document.getElementById("symbol").innerHTML=turn[i%2]
           win();
         });
       
         grid9.addEventListener("click", () => {
+          act.play();
           document.getElementById("grid9").innerHTML = turn[i % 2];
           g9 = turn[i % 2];
           i++;
           grid9.classList.remove("hidden");
           grid9.classList.add("used");
+          document.getElementById("symbol").innerHTML=turn[i%2]
           win();
         });
       }
@@ -128,12 +161,16 @@ startBtn.addEventListener('click',()=>{
 function win() {
 
 if(i==9){
-        console.log("draw")
+      draw.play();
+        winner = 1
+        game = false;
     }
 
   if (g1 === "X" && g2 === "X" && g3 === "X") {
+    
     game = false;
-    console.log("USER 1 WON");
+    winaudio.play();
+    winner =1;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -146,7 +183,8 @@ if(i==9){
   }
   if (g4 === "X" && g5 === "X" && g6 === "X") {
     game = false;
-    console.log("USER 1 WON");
+    winaudio.play();
+    winner =1;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -159,7 +197,8 @@ if(i==9){
   }
   if (g7 === "X" && g8 === "X" && g9 === "X") {
     game = false;
-    console.log("USER 1 WON");
+    winaudio.play();
+    winner =1;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -172,7 +211,8 @@ if(i==9){
   }
   if (g1 === "X" && g4 === "X" && g7 === "X") {
     game = false;
-    console.log("USER 1 WON");
+    winaudio.play();
+    winner =1;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -185,7 +225,8 @@ if(i==9){
   }
   if (g2 === "X" && g5 === "X" && g8 === "X") {
     game = false;
-    console.log("USER 1 WON");
+    winaudio.play();
+    winner =1;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -198,7 +239,8 @@ if(i==9){
   }
   if (g3 === "X" && g6 === "X" && g9 === "X") {
     game = false;
-    console.log("USER 1 WON");
+    winaudio.play();
+    winner =1;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -211,7 +253,8 @@ if(i==9){
   }
   if (g1 === "X" && g5 === "X" && g9 === "X") {
     game = false;
-    console.log("USER 1 WON");
+    winaudio.play();
+    winner =1;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -224,7 +267,8 @@ if(i==9){
   }
   if (g3 === "X" && g5 === "X" && g7 === "X") {
     game = false;
-    console.log("USER 1 WON");
+    winaudio.play();
+    winner =1;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -238,7 +282,8 @@ if(i==9){
   //For User 2
   if (g1 === "O" && g2 === "O" && g3 === "O") {
     game = false;
-    console.log("USER 2 WON");
+    winaudio.play();
+    winner=2;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -251,7 +296,8 @@ if(i==9){
   }
   if (g4 === "O" && g5 === "O" && g6 === "O") {
     game = false;
-    console.log("USER 2 WON");
+    winaudio.play();
+    winner=2;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -264,7 +310,8 @@ if(i==9){
   }
   if (g7 === "O" && g8 === "O" && g9 === "O") {
     game = false;
-    console.log("USER 2 WON");
+    winaudio.play();
+    winner=2;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -277,7 +324,8 @@ if(i==9){
   }
   if (g1 === "O" && g4 === "O" && g7 === "O") {
     game = false;
-    console.log("USER 2 WON");
+    winaudio.play();
+    winner=2;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -290,7 +338,8 @@ if(i==9){
   }
   if (g2 === "O" && g5 === "O" && g8 === "O") {
     game = false;
-    console.log("USER 2 WON");
+    winaudio.play();
+    winner=2;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -303,7 +352,8 @@ if(i==9){
   }
   if (g3 === "O" && g6 === "O" && g9 === "O") {
     game = false;
-    console.log("USER 2 WON");
+    winaudio.play();
+    winner=2;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -316,7 +366,8 @@ if(i==9){
   }
   if (g1 === "O" && g5 === "O" && g9 === "O") {
     game = false;
-    console.log("USER 2 WON");
+    winaudio.play();
+    winner=2;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -329,7 +380,8 @@ if(i==9){
   }
   if (g3 === "O" && g5 === "O" && g7 === "O") {
     game = false;
-    console.log("USER 2 WON");
+    winaudio.play();
+    winner=2;
     grid1.classList.add("used");
     grid2.classList.add("used");
     grid3.classList.add("used");
@@ -341,6 +393,8 @@ if(i==9){
     grid9.classList.add("used");
   }
 }
+
+
 
 
 
